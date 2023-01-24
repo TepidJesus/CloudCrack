b341c815c56264ceb632222aaaf2473826312a31
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2023-01-24 21:49:40 +1300

    Further Testing

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import boto3;
 
 # Create an ec2 resource
 ec2 = boto3.resource('ec2')
-
-for instance in ec2.instances.all():
-    print(instance.id, instance.state)
+AMI_ID = 'ami-0d'
+# Create a new P4 Large EC2 instance with the AMI ID
+instances = ec2.create_instances(
+    ImageId=AMI_ID,
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='p4d.24xlarge',
+    ) 
