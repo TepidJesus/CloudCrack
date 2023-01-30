807733d40f7a00905aa3f8a91df4652c13f88980
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2023-01-30 19:18:45 +1300

    Started Work on Setup Code

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import boto3;
 
+SETUP_COMPLETED = False
+
+# Create a boto3 client to interact with AWS services that accepts the AWS credentials as parameters
+def create_client(service_name, aws_access_key_id, aws_secret_access_key):
+    return boto3.client(
+        service_name,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
 
 
-# Create an ec2 resource
-ec2 = boto3.resource('ec2')
-AMI_ID = 'ami-0d'
-# Create a new P4 Large EC2 instance with the AMI ID
-instances = ec2.create_instances(
-    ImageId=AMI_ID,
-    MinCount=1,
-    MaxCount=1,
-    InstanceType='p4d.24xlarge',
-    ) 
+
+
+print("Welcome to EZ Cracker!")
+
+if (not SETUP_COMPLETED): # Placeholder for now, need to add detection for presence of credentials in .env file
+    print("Looks like you haven't set up your AWS credentials yet. Let's do that now.")
+    aws_access_key_id = input("Enter your AWS Access Key ID: ")
+    aws_secret_access_key = input("Enter your AWS Secret Access Key: ")
+    print("Please wait while I validate your credentials...")
+    
+    client = create_client("iam", aws_access_key_id, aws_secret_access_key)
+
