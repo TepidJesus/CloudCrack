23fa5d07ee098411f56fbc6b1444a663662c8ace
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2023-02-06 11:37:23 +1300

    Setup Process Completed

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import boto3;
 import os
 import dotenv
+from botocore.exceptions import ClientError
 
-SETUP_COMPLETED = False
-TEST_AMI_ID = "ami-05bfbece1ed5beb54"
+TEST_AMI_ID = "ami-05bfbece1ed5beb54" # Ubuntu 18.04 AMI
 
+def dotenv_present():
+    try:
+        with open(".env", "r") as f:
+            dotenv = f.read()
+        return True
+    except:
+        return False
 
-
-
-
+def set_credentials(aws_access_key_id, aws_secret_access_key):
+    with open(".env", "w") as f:
+        f.write("AWS_ACCESS_KEY_ID=" + aws_access_key_id)
+        f.write("\nAWS_SECRET_ACCESS_KEY=" + aws_secret_access_key)
 
 print("Welcome to EZ Cracker!")
 
-if (not SETUP_COMPLETED): # Placeholder for now, need to add detection for presence of credentials in .env file
+if (not dotenv_present()):
     print("Looks like you haven't set up your AWS credentials yet. Let's do that now.")
     aws_access_key_id = input("Enter your AWS Access Key ID: ")
     aws_secret_access_key = input("Enter your AWS Secret Access Key: ")
@@ -21,29 +29,33 @@ if (not SETUP_COMPLETED): # Placeholder for now, need to add detection for prese
     try:
         client = boto3.client('ec2', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name='us-east-2')
         client.run_instances(ImageId=TEST_AMI_ID, MinCount=1, MaxCount=1, InstanceType='t2.micro', DryRun=True)
-    except DryRunOperation:
-        SETUP_COMPLETED = True
-        print("Success! Your credentials are valid.")
+    except ClientError as e:
+        if 'DryRunOperation' not in str(e):
+            print("Error: Your credentials are invalid. Please make sure you entered them correctly.")
+            raise e
+        else:
+            print("Success! Your credentials are valid.")
+            set_credentials(aws_access_key_id, aws_secret_access_key)
     except:
-        print("Error: Your credentials are invalid. Please make sure you entered them correctly.")
+        print("Sorry, there was an error validating your credentials. Please try again.")
         exit()
 
-else:
-    try:
-        dotenv.load_dotenv()
-        aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
-        aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-    except:
-        print("Error: Your credentials are invalid. Please make sure the credentials in your .env file are correct.")
-        exit()
 
-    print("Looks like you've already set up your AWS credentials. Let's get started!")
-    _hash = input("Enter the hash you want to crack: ")
-    print("Please wait while I crack your hash...")
-    sqs = boto3.resource('sqs')
-    queue = sqs.create_queue(QueueName='hash_queue', Attributes={'DelaySeconds': '5'})
-    print(queue.url)
-    queue.delete()
+try:
+    dotenv.load_dotenv()
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+except:
+    print("Error: Your credentials are invalid. Please make sure the credentials in your .env file are correct.")
+    exit()
+
+
+_hash = input("Enter the hash you want to crack: ")
+print("Please wait while I crack your hash...")
+sqs = boto3.resource('sqs')
+queue = sqs.create_queue(QueueName='hash_queue', Attributes={'DelaySeconds': '5'})
+print(queue.url)
+queue.delete()
     
     # try:
     # except:    
@@ -54,6 +66,7 @@ else:
     # Create an EC2 instance to crack the hash
     # Wait for the EC2 instance to crack the hash
     # Print the cracked hash
+
 
 
 
