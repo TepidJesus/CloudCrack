0a306d806c0a7251b0dcf579d9b067368249c428
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2023-02-12 14:19:31 +1300

    Added CLI arguments

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import dotenv
 from botocore.exceptions import ClientError
 
+import sys, getopt
+
 TEST_AMI_ID = "ami-05bfbece1ed5beb54" # Ubuntu 18.04 AMI
 
 def dotenv_present():
@@ -17,6 +19,32 @@ def set_credentials(aws_access_key_id, aws_secret_access_key):
     with open(".env", "w") as f:
         f.write("AWS_ACCESS_KEY_ID=" + aws_access_key_id)
         f.write("\nAWS_SECRET_ACCESS_KEY=" + aws_secret_access_key)
+
+argv = sys.argv[1:]
+opts, args = getopt.getopt(argv, "hiIt:w", ["help", "inputHash", "inputFile", "hashType", "wordlist"])
+
+for opt, arg in opts:
+    if opt in ("-h", "--help"):
+        print("main.py -i <inputHash> -I <inputFile> -t <hashType>, -w <wordlist>\n")
+        print("-h Show This Menue\n")
+        print("-i <inputHash> Enter single a hash to crack\n")
+        print("-I <inputFile> Enter a file with hashes to crack\n   e.g /home/user/Desktop/hashes.txt\n")
+        print("-t <hashType> Enter the type of hash to crack\n")
+        print("-w <wordlist> Enter the name of a wordlist from the Seclists repository to use (e.g rockyou.txt)")
+        sys.exit()
+    elif opt in ("-i", "--inputHash"):
+        print("Input Hash")
+        user_hash = arg
+    elif opt in ("-I", "--inputFile"):
+        print("Input File")
+        hash_file = arg
+    elif opt in ("-t", "--hashType"):
+        hash_type = arg
+        print("Hash Type")
+    elif opt in ("-w", "--wordlist"):
+        wordlist = arg
+        print("Wordlist")
+
 
 print("Welcome to EZ Cracker!")
 
@@ -52,20 +80,12 @@ except:
 
 _hash = input("Enter the hash you want to crack: ")
 print("Please wait while I crack your hash...")
+
 sqs = boto3.resource('sqs')
 queue = sqs.create_queue(QueueName='hash_queue', Attributes={'DelaySeconds': '5'})
 print(queue.url)
 queue.delete()
-    
-    # try:
-    # except:    
-    #     print("Error: There was an error creating the SQS queue. Please try again.")
-    #     raise SystemExit
 
-    # Create an SQS queue to send this hash to an EC2 instance
-    # Create an EC2 instance to crack the hash
-    # Wait for the EC2 instance to crack the hash
-    # Print the cracked hash
 
 
 
