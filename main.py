bf7b9a0a3f1aa9083d0d778c05e6a45966819d14
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2023-02-13 18:57:22 +1300

    Changing setup system

--- main.py
+++ main.py
@@ -20,42 +20,9 @@ def set_credentials(aws_access_key_id, aws_secret_access_key):
         f.write("AWS_ACCESS_KEY_ID=" + aws_access_key_id)
         f.write("\nAWS_SECRET_ACCESS_KEY=" + aws_secret_access_key)
 
-argv = sys.argv[1:]
-opts, args = getopt.getopt(argv, "hiIt:wo", ["help", "inputHash", "hashFile", "hashType", "wordlist", "outputFile"])
-
-for opt, arg in opts:
-    if opt in ("-h", "--help"):
-        print("Example: main.py -i <inputHash> -I <inputFile> -t <hashType>, -w <wordlist>\n")
-        print("Options:")
-        print("     -h, --help                        Show This Menue")
-        print("     -i, --inputHash      <inputHash>  The hash to be cracked")
-        print("     -I, --hashFile       <inputFile>  The loaction of a txt file with a list of hashes to crack  (e.g /home/user/Desktop/hashes.txt)")
-        print("     -t, --hashType       <hashType>   The type of hash to be cracked (e.g md5, sha1, sha256, sha512)")
-        print("     -w, --wordlist       <wordlist>   The name of a wordlist from the Seclists repository (e.g rockyou.txt). The repository can be found at https://github.com/danielmiessler/SecLists/tree/master/Passwords")
-        print("     -o, --outputFile     <outputFile> The location of the output file (e.g /home/user/Desktop/output.txt). Will output to the console if not specified.")
-        sys.exit()
-    elif opt in ("-i", "--inputHash"):
-        print("Input Hash is: " + arg)
-        user_hash = arg
-    elif opt in ("-I", "--hashFile"):
-        print("Input File is: " + arg)
-        hash_file = arg
-    elif opt in ("-t", "--hashType"):
-        hash_type = arg
-        print("Hash Type is: " + arg)
-    elif opt in ("-w", "--wordlist"):
-        wordlist = arg
-        print("Wordlist is: " + arg + "")
-    elif opt in ("-o", "--outputFile"):
-        output_file = arg
-        print("Output File is: " + arg + "")
-        sys.exit()
-
-
-print("Welcome to EZ Cracker!")
-
-if (not dotenv_present()):
-    print("Looks like you haven't set up your AWS credentials yet. Let's do that now.")
+def run_setup():
+    print("Welcome to the EZ Cracker setup wizard!")
+    print("Lets get started by setting up your AWS credentials. You can find these instructions for this in the setup guide in the README.md file.")
     aws_access_key_id = input("Enter your AWS Access Key ID: ")
     aws_secret_access_key = input("Enter your AWS Secret Access Key: ")
     print("Please wait while I validate your credentials...")
@@ -74,6 +41,48 @@ if (not dotenv_present()):
         print("Sorry, there was an error validating your credentials. Please try again.")
         exit()
 
+argv = sys.argv[1:]
+opts, args = getopt.getopt(argv, "hi:I:t:w:o:s", ["help", "inputHash", "hashFile", "hashType", "wordlist", "outputFile", "setup"])
+
+for opt, arg in opts:
+    if opt in ("-h", "--help"):
+        print("Example: main.py -i <inputHash> -I <inputFile> -t <hashType>, -w <wordlist>\n")
+        print("Options:")
+        print("     -h, --help                        Show This Menue")
+        print("     -i, --inputHash      <inputHash>  The hash to be cracked")
+        print("     -I, --hashFile       <inputFile>  The loaction of a txt file with a list of hashes to crack  (e.g /home/user/Desktop/hashes.txt)")
+        print("     -t, --hashType       <hashType>   The type of hash to be cracked (e.g md5, sha1, sha256, sha512)")
+        print("     -w, --wordlist       <wordlist>   The name of a wordlist from the Seclists repository (e.g rockyou.txt). The repository can be found at https://github.com/danielmiessler/SecLists/tree/master/Passwords")
+        print("     -o, --outputFile     <outputFile> The location of the output file (e.g /home/user/Desktop/output.txt). Will output to the console if not specified.")
+        print("     -s, --setup                      Run the setup wizard")
+        sys.exit()
+    elif opt in ("-s", "--setup"):
+        run_setup()
+    elif opt in ("-i", "--inputHash"):
+        print("Input Hash is: " + arg)
+        user_hash = arg
+    elif opt in ("-I", "--hashFile"):
+        print("Input File is: " + arg)
+        hash_file = arg
+    elif opt in ("-t", "--hashType"):
+        hash_type = arg
+        print("Hash Type is: " + arg)
+    elif opt in ("-w", "--wordlist"):
+        wordlist = arg
+        print("Wordlist is: " + arg + "")
+    elif opt in ("-o", "--outputFile"):
+        output_file = arg
+        print("Output File is: " + arg + "")
+        sys.exit()
+    else:
+        print("Invalid Option. Please use -h or --help for more information.")
+
+
+print("Welcome to EZ Cracker!")
+
+if (not dotenv_present()):
+    print("Error: You have not set up your AWS credentials. Please run the setup wizard by using the -s or --setup flag.")
+    exit()
 
 try:
     dotenv.load_dotenv()
@@ -83,7 +92,8 @@ except:
     print("Error: Your credentials are invalid. Please make sure the credentials in your .env file are correct.")
     exit()
 
-
+if hash_file == None or hash_file == "":
+    
 _hash = input("Enter the hash you want to crack: ")
 print("Please wait while I crack your hash...")
 
