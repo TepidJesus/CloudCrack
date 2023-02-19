a5a80193b6d12428fb1326dd4f31cdc792ab2242
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2023-02-20 11:13:55 +1300

    Fleshing Out Command/Job

--- job_handler.py
+++ job_handler.py
@@ -7,27 +7,69 @@ class JobHandler:
         self.control_queue = control_queue
         self.return_queue = return_queue
 
+        self.job_id = 1
         self.job_log = {}
 
+    def send_job(self, job):
+        self.delivery_queue.send_message(MessageBody=job.to_json())
+
+    def get_new_job_id(self):
+        num = self.job_number
+        self.job_number += 1
+        return num
+    
+    def create_job(self, _hash, hash_type, attack_mode, required_info):
+        job = Job(self.get_new_job_id(), _hash, hash_type, attack_mode, required_info)
+        self.job_log[job.job_id] = job
+        return job
+
+class STATUS:
+    CREATED = 1,
+    RUNNING = 2,
+    FINISHED = 3,
+    FAILED = 4,
+    CANCELLED = 5,
+    SENT = 6
  
 class Job:
 
-    def __init__(self, job_id, _hash, wordlist, hash_type, attack_mode):
+    def __init__(self, job_id, _hash, hash_type, attack_mode, required_info):
         self.job_id = job_id
         self.hash = _hash
-        self.wordlist = wordlist
         self.hash_type = hash_type
-        self.job_status = 'created'
+        self.job_status = STATUS.CREATED
         self.attack_mode = attack_mode
+        self.required_info = required_info
 
     def __str__(self):
-        return f"Job ID: {self.job_id} | Hash: {self.hash} | Wordlist: {self.wordlist} | Hash Type: {self.hash_type} | Job Status: {self.job_status}"
+        return f"Job ID: {self.job_id} | Hash: {self.hash} | Hash Type: {self.hash_type} | Job Status: {self.job_status}"
     
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
     
     def from_json(self, json_string):
         return json.loads(json_string)
+    
+
+class Command:
+
+    def __init__(self, job_id, request):
+        self.job_id = job_id
+        self.request = request
+
+    def __str__(self):
+        return f"Job ID: {self.job_id} | Job Status: {self.job_status}"
+    
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+    
+    def from_json(self, json_string):
+        return json.loads(json_string)
+    
+        
+    
+
+
 
 
         
