dcbc30db53bc4660081f21ea4163a0aa31dfdf71
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2023-02-22 14:02:19 +1300

    Rearraged

--- job_handler.py
+++ job_handler.py
@@ -1,5 +1,34 @@
 import json
 ### HashCat Command Format: hashcat -a <attack_mode> -m <hash_type> <hash> <wordlist/mask/length> -w 4
+
+class STATUS:
+    CREATED = 1,
+    RUNNING = 2,
+    FINISHED = 3,
+    FAILED = 4,
+    CANCELLED = 5,
+    SENT = 6
+
+class Job:
+
+    def __init__(self, job_id, _hash, hash_type, attack_mode, required_info):
+        self.job_id = job_id
+        self.hash = _hash
+        self.hash_type = hash_type
+        self.job_status = STATUS.CREATED
+        self.attack_mode = attack_mode
+        self.required_info = required_info
+
+    def __str__(self):
+        return f"Job ID: {self.job_id} | Hash: {self.hash} | Hash Type: {self.hash_type} | Job Status: {self.job_status}"
+    
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+    
+    def from_json(self, json_string):
+        return json.loads(json_string)
+
+
 class JobHandler:
 
     def __init__(self, delivery_queue, control_queue, return_queue):
@@ -25,7 +54,7 @@ class JobHandler:
     
     def cancel_job(self, job):
         self.job_log[job.job_id].job_status = STATUS.CANCELLED
-        self.control_queue.send_message(MessageBody=Command(job.job_id, STATUS.CANCELLED).to_json())
+        self.control_queue.send_message(MessageBody=Command(job.job_id, REQUEST.CANCEL).to_json())
     
     def get_job_status(self, job):
         return self.job_log[job.job_id].job_status
@@ -37,33 +66,10 @@ class JobHandler:
         else:
             return None
 
-class STATUS:
-    CREATED = 1,
-    RUNNING = 2,
-    FINISHED = 3,
-    FAILED = 4,
-    CANCELLED = 5,
-    SENT = 6
+class REQUEST:
+    CANCEL = 1,
+    STATUS = 2,
  
-class Job:
-
-    def __init__(self, job_id, _hash, hash_type, attack_mode, required_info):
-        self.job_id = job_id
-        self.hash = _hash
-        self.hash_type = hash_type
-        self.job_status = STATUS.CREATED
-        self.attack_mode = attack_mode
-        self.required_info = required_info
-
-    def __str__(self):
-        return f"Job ID: {self.job_id} | Hash: {self.hash} | Hash Type: {self.hash_type} | Job Status: {self.job_status}"
-    
-    def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
-    
-    def from_json(self, json_string):
-        return json.loads(json_string)
-    
 
 class Command:
 
@@ -80,6 +86,7 @@ class Command:
     def from_json(self, json_string):
         return json.loads(json_string)
     
+        
         
     
 
