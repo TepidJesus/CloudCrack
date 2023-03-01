b943d30bd46d31e7926105142bf05c33fc3ddcd0
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2023-03-01 20:12:57 +1300

    Remade Credential System

--- server-side/receiver.py
+++ server-side/receiver.py
@@ -1,6 +1,8 @@
 #from sh import hashcat
 import boto3
 import time
+import os
+import dotenv
 from cat_handler import HashcatHandler
 import sys
 sys.path.insert(0, "../")
@@ -10,8 +12,8 @@ from job_handler import STATUS
 ### HashCat Command Format: hashcat -a <attack_mode> -m <hash_type> <hash> <wordlist/mask/length> -w 4
 
 ## Check For Two aws SQS Queues and assign them to variables
-def get_infrastructure():
-    sqs = boto3.resource('sqs')
+def get_infrastructure(session):
+    sqs = session.resource('sqs')
     delivery = sqs.get_queue_by_name(QueueName='deliveryQueue.fifo')
     control = sqs.get_queue_by_name(QueueName='controlQueue.fifo')
     return_queue = sqs.get_queue_by_name(QueueName='returnQueue.fifo')
@@ -24,26 +26,18 @@ def check_queue(queue):
         return messages[0]
     return None
 
-# def load_job(job, job_queue):
-#     # job_as_command
-#     if job_queue.len() == 0:
-#         hashcat('-a', job.attack_mode, '-m', job.hash_type, job.hash, job.required_info, '-w', '4')
-#     else:
-#         job_queue.append(job)
-
-def complete_job_test(job): ### For testing message responses between consumer and producer
-    print("Job Complete")
-    job.job_status = STATUS.COMPLETED
-    job.required_info = {"hash": job.hash, "dehashed": "password"}
-    return job
-
-
-## Send a message to the control queue
-
-
-
 def main():
-    delivery, control, return_queue = get_infrastructure()
+
+    try:
+        dotenv.load_dotenv()
+        aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+        aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+        session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    except:
+        print("Error: Your credentials are invalid. Run --setup again if your credentials have changed.")
+        exit()    
+
+    delivery, control, return_queue = get_infrastructure(session)
     cat_handler = HashcatHandler(return_queue, control, delivery)
     print("Receiver is running...")
     while True:
@@ -64,9 +58,6 @@ def main():
             print(job.to_json())
             new_job.delete()
             time.sleep(5)
-            comp_job = complete_job_test(job)
-            print("Returning Job")
-            cat_handler.return_job(comp_job)
             time.sleep(1)
         time.sleep(5)
 
