397bf65c61c85c9315154b1772e5e8e873270739
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2023-05-15 09:42:39 +1200

    Auto Instance Shutdown

--- server-side/receiver.py
+++ server-side/receiver.py
@@ -8,6 +8,7 @@ sys.path.insert(0, "../")
 from job_handler import STATUS, REQUEST
 import json
 from client import AwsController
+import requests
 
 ### HashCat Command Format: hashcat -a <attack_mode> -m <hash_type> <hash> <wordlist/mask/length> -w 4
 
@@ -43,34 +44,51 @@ def main():
         aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
         aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
         session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name='us-east-2')
+        response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
+        instance_id = response.text
+        
     except:
         print("Error: Your credentials are invalid. Run --setup again if your credentials have changed.")
+        ec2_client = session.client('ec2')
+        ec2_client.terminate_instances(InstanceIds=[instance_id])
         exit()    
 
     delivery, control, return_queue, s3_client = get_infrastructure(session)
     cat_handler = HashcatHandler(AwsController(get_config(), "server"))
     print("Receiver is running...")
+    retries = 0
     while True:
+        try:
+            command = check_queue(control)
+            print("Checking for new commands...")
 
-        command = check_queue(control)
-        print("Checking for new commands...")
-
-        if command != None:
-            commandJs = json.loads(command.body)
-            if commandJs["request"] == REQUEST.CANCEL:
-                print("Cancel command received!")
-                cat_handler.cancel_job(int(commandJs["job_id"]))
-            command.delete()
-        
-        if cat_handler.current_job is None:
-            new_job = check_queue(delivery)
-            print("Checking for new jobs...")
-            if new_job != None:
-                print("New job found!")
-                job = cat_handler.load_from_json(new_job.body)
-                print(job.to_json())
-                cat_handler.run_job(job)
-                new_job.delete()
-        time.sleep(5)
+            if command != None:
+                commandJs = json.loads(command.body)
+                if commandJs["request"] == REQUEST.CANCEL:
+                    print("Cancel command received!")
+                    cat_handler.cancel_job(int(commandJs["job_id"]))
+                command.delete()
+            
+            if cat_handler.current_job is None:
+                new_job = check_queue(delivery)
+                print("Checking for new jobs...")
+                if new_job != None:
+                    retries = 0
+                    print("New job found!")
+                    job = cat_handler.load_from_json(new_job.body)
+                    print(job.to_json())
+                    cat_handler.run_job(job)
+                    new_job.delete()
+                elif retries < 5:
+                    retries += 1
+                    print("No new jobs found. Retrying...")
+                else:
+                    raise Exception("No new jobs found.")
+            time.sleep(5)
+        except:
+            print("Error: Something went wrong. Check the logs for more details.")
+            ec2_client = session.client('ec2')
+            ec2_client.terminate_instances(InstanceIds=[instance_id])
+            exit()
 
 main()
