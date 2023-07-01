ae0800383306ab8f2407e199b28897f4b2473bea
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2023-07-01 19:24:57 +1200

    Added MalformedJobError

--- errors.py
+++ errors.py
@@ -2,3 +2,6 @@ class MaskFormatError(Exception):
     def __init__(self, message) -> None:
         super().__init__(message)
 
+class MalformedJobError(Exception):
+    def __init__(self, message) -> None:
+        super().__init__(message)
